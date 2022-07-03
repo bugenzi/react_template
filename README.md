86e626fbc0abffead41989b75e7394b21b172b9c
Amar Bugarin <amarbugarin@outlook.com>
2022-07-03 20:33:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Boilerplate
+
+## To run
+
+- `npm run client` **To run the react server**
+- `npm run build` **Babel compile of the buil saved it build folder**
+- `npm run test` **Jest TDD**
+- `npm run lint` **Manualty lint**
---
+++ webpack.dev.config.ts
@@ -0,0 +1,67 @@
+import path from "path";
+import {
+  HotModuleReplacementPlugin,
+  Configuration as WebpackConfiguration,
+} from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import ESLintPlugin from "eslint-webpack-plugin";
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const config: Configuration = {
+  stats: {
+    children: true,
+  },
+  mode: "development",
+  output: {
+    publicPath: "/",
+  },
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new HotModuleReplacementPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+    new ESLintPlugin({
+      extensions: ["js", "jsx", "ts", "tsx"],
+    }),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    static: path.join(__dirname, "build"),
+    historyApiFallback: true,
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+};
+
+export default config;
